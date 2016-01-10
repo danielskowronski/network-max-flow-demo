8b990f9fb41cb78d2e19583079afec5bea44a799
Daniel Skowroński <daniel@dsinf.net>
2016-01-10 22:26:18 +0100

    MSAGL added

---
+++ MaxFlowDemo/Form1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Msagl.Drawing;
+using Microsoft.Msagl.GraphViewerGdi;
+using Microsoft.Msagl.Layout.Layered;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System;
+
+namespace MaxFlowDemo
+{
+    public class EdgeData
+    {
+       
+    }
+    public partial class Form1 : Form
+    {
+       
+    }
+}
